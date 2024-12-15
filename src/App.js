3f832ad1993a3259c5778660bec37486969d90e3
shiridisai-spec <shirdisai371@gmail.com>
2024-12-15 11:32:31 +0530

    router fixes!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Routes,
   Navigate,
